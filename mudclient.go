788426a06c85a3343ea5148e90e9c4216cb2302c
mjw <mattwagner@us.ibm.com>
2015-05-05 13:48:33 -0400

    Initial commit of a basic MUD client which picks up on input to run scripts

---
+++ mudclient.go
@@ -0,0 +1,115 @@
+package main 
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"strings"
+)
+
+type MudConnection struct {
+	Conn net.Conn
+	RunningScripts []Script
+}
+
+func main() {
+	conn, err := net.Dial("tcp", "legendsofthejedi.com:5656")
+//	conn, err := net.Dial("tcp", "arkmud.org:4200")
+	if err != nil { log.Fatalf("Error connecting: %s", err.Error()) }
+	
+	mudConn := new(MudConnection)
+	mudConn.Conn = conn
+	mudConn.RunningScripts = make([]Script, 0)
+	mudConn.startScanners()
+}
+
+func (mudConn *MudConnection) startScanners() {
+	go mudConn.startInputScanner()
+	mudConn.startOutputScanner()
+}
+
+func (mudConn *MudConnection) startOutputScanner() {
+	scanner := bufio.NewScanner(mudConn.Conn)
+	scanner.Split(bufio.ScanBytes)
+	scannerChar := ""
+	lineBuf := ""
+	
+	for scanner.Scan() {
+		if scanner.Err() != nil {
+			log.Fatalf("Error reading from server: %s", scanner.Err().Error())
+		} else {
+			scannerChar = scanner.Text()
+			fmt.Print(scannerChar)
+			
+			if scannerChar[0] == 13 {
+				if len(lineBuf) > 0 {
+					mudConn.checkOutputForTriggers(lineBuf)
+					lineBuf = ""
+				}
+			} else {
+				lineBuf = fmt.Sprintf("%s%s", lineBuf, scannerChar)
+			}
+		}
+	}
+}
+
+func (mudConn *MudConnection) startInputScanner() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		commandLine := scanner.Text()
+		if !mudConn.interceptInput(commandLine) {
+			mudConn.sendLine(commandLine)
+		}
+	}
+}
+
+func (mudConn *MudConnection) sendLine(line string) {
+	fmt.Printf("<< %s\n", line)
+	_, err := mudConn.Conn.Write([]byte(fmt.Sprintf("%s\n\r", line)))
+	if err != nil { log.Fatalf("Error writing: %s", err.Error()) }
+}
+
+func (mudConn *MudConnection) checkOutputForTriggers(line string) {
+	// Sanitize the line - remove all color codes, newline, etc.
+	escaping := false
+	cleanLine := ""
+	for i := 0; i < len(line); i++ {
+		if line[i] == 0x1B {
+			escaping = true
+		} else {
+			if !escaping {
+				cleanLine = fmt.Sprintf("%s%c", cleanLine, line[i])
+			} else {
+				if line[i] == 'm' {
+					escaping = false
+				}
+			}
+		}
+	}
+	
+	line = strings.TrimSpace(cleanLine)
+	
+	for _, script := range mudConn.RunningScripts {
+		script.SendOutput(line)
+	}
+}
+
+func (mudConn *MudConnection) interceptInput(line string) bool {
+	if len(strings.TrimSpace(line)) == 0 {
+		return false
+	}
+
+	args := strings.Fields(line)[1:]
+	intercepted := true
+	switch line {
+		case "autoresearch":
+			script := &ResearchScript{&BaseScript{}}
+			script.Execute(args, script, mudConn)
+		default:
+			intercepted = false
+	}
+	
+	return intercepted
+}
---
+++ scripts.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+
+func GetOutputUntilPrompt(mudOutput chan string) (cmdOutput string) {
+	cmdOutput = ""
+	LineLoop:
+		for {
+			select {
+				case line := <-mudOutput:
+					if strings.HasPrefix(line, "HP: ") && strings.HasSuffix(line, ">") {
+						break LineLoop
+					}
+					cmdOutput = cmdOutput+"\n"+line
+			}
+		}
+
+	return cmdOutput
+}
+
+type Script interface {
+	Execute([]string, Script, *MudConnection)
+	innerExecute([]string)
+	SendOutput(string)
+}
+
+type BaseScript struct {
+	Script
+	MudOutput chan string
+	MudConn *MudConnection
+}
+
+func (script *BaseScript) Execute(args []string, outerScript Script, mudConn *MudConnection) {
+	script.MudOutput = make(chan string)
+	script.MudConn = mudConn
+
+	mudConn.RunningScripts = append(mudConn.RunningScripts, outerScript)
+	go script.invokeInnerExecute(args, outerScript)
+}
+
+func (script *BaseScript) invokeInnerExecute(args []string, outerScript Script) {
+	outerScript.innerExecute(args)
+	s := script.MudConn.RunningScripts
+	
+	for i := 0; i < len(s); i++ {
+		if outerScript == s[i] {
+		    if i < len(s)-1 {
+		        script.MudConn.RunningScripts = append(s[:i], s[i+1:]...)
+		    } else {
+		        script.MudConn.RunningScripts = s[:i]
+		    }
+			return
+		}
+	}
+	fmt.Errorf("Could not find running script in scripts collection")
+}
+
+func (script *BaseScript) SendOutput(line string) {
+	script.MudOutput <- line
+}
+
+
+type ResearchScript struct {
+	*BaseScript
+}
+
+func (script *ResearchScript) innerExecute(args []string) {
+	researchQueue := make([]string, 0)
+
+	script.MudConn.sendLine("practice")
+	practiceOutput := GetOutputUntilPrompt(script.MudOutput)
+	for _, skillLine := range strings.Split(practiceOutput, "\n") {
+		if strings.Contains(skillLine, "%") {
+			for _, skill := range strings.Split(skillLine, "%") {
+				skill = strings.TrimSpace(skill)
+				if len(skill) > 0 {
+					skillParts := strings.Split(skill, "  ")
+					skillLevel, err := strconv.ParseInt(skillParts[1], 0, 0)
+					if err != nil { fmt.Errorf("Error in parseInt: %s", err.Error()) }
+					
+					if (skillLevel < 76) {
+						researchQueue = append(researchQueue, skillParts[0])
+					}
+				}
+			}
+		}
+	}
+	
+	for _, skill := range researchQueue {
+		script.MudConn.sendLine(fmt.Sprintf("research %s", skill))
+
+		ResearchLoop:
+		for {
+			select {
+				case line := <-script.MudOutput:
+					if strings.Contains(line, "You can't learn ") {
+						break ResearchLoop
+					} else if line == "You study for hours on end, but fail to gather any knowledge." || line == "You finish your studies and feel much more skilled." {
+						script.MudConn.sendLine(fmt.Sprintf("research %s", skill))
+					}
+			}
+		}
+	}
+}
